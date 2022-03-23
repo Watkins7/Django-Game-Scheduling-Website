660c2c5e97d12acd3219cec8a0979c0e6c2aa317
Thaddeus <71716327+Watkins7@users.noreply.github.com>
2022-03-23 11:50:36 -0400

    update form.py
    -Included labels so the form can be displayed properly
    -Changed Libraries
    -Altered fields due to Model changes

--- pick_up_app/forms.py
+++ pick_up_app/forms.py
@@ -1,19 +1,29 @@
-# Libraries and classes needed to make registration class
-from django.contrib.auth.models import User
-from django import forms
-from django.core.exceptions import ValidationError
-from .models import PickupTeam
-from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 
+# Team Table
+from .models import PickupTeam
 
-# Registration Class
+
+# Registration Form
 class NewPickupUserForm(ModelForm):
-
     class Meta:
         model = PickupTeam
-        fields = '__all__'
-    #PickupTeam.longitude = forms.FloatField(label='Longitude!:',max_value=100)
-    #PickupTeam.latitude = forms.FloatField(label='Latitude!:', max_value=100)
-    #PickupTeam.email = forms.CharField(label='Email!:')
 
+        # These are the attributes to be stored
+        fields = (
+            'teamname',
+            'email',
+            'password',
+            'checkpassword'
+            'longitude',
+            'latitude')
+
+        # This is what the form displays them as
+        labels = {
+            'teamname': "New Team Name:",
+            'email': "Registration Email:",
+            'password': "Password:",
+            'checkpassword': "Password Conformation:",
+            'longitude': "Longitude:",
+            'latitude': "Latitude:"
+        }
