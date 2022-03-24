01a2c056121ed8ce9d992c30f89772204260c14f
Thaddeus <watkins7@umbc.edu>
2022-03-24 15:42:59 -0400

    -added foreignkey 1 to 1, linking custom model 'PickupTeam' to 'User' -Note: User and Team are linked, therefore it can use 'User' model

--- pick_up_app/models.py
+++ pick_up_app/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 
 class PickupTeam(models.Model):
-    username = models.CharField(max_length=50,default='')
-    email = models.CharField(max_length=200,default='')
+
+    teamname = models.CharField(max_length=50,default='')
+    email = models.EmailField(max_length=100, default='')
     password = models.CharField(max_length=50,default='')
-    longitude = models.FloatField(default=0)
-    latitude = models.FloatField(default=0)
+    checkpassword = models.CharField(max_length=50, default='')
+    longitude = models.FloatField(default=76.7100)
+    latitude = models.FloatField(default=39.2543)
+
+    teamaccount = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
