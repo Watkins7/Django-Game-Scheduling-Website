423a4935d27e47153eb15054d617f99632f98922
Thaddeus <watkins7@umbc.edu>
2022-03-24 15:41:02 -0400

    -commented form.py

--- pick_up_app/forms.py
+++ pick_up_app/forms.py
@@ -7,9 +7,9 @@ from .models import PickupTeam
 # Error Handling
 from django.core.exceptions import ValidationError
 
+
 # Registration Form
 class NewPickupUserForm(ModelForm):
-
     class Meta:
         model = PickupTeam
 
@@ -32,26 +32,36 @@ class NewPickupUserForm(ModelForm):
             'latitude': "Latitude:",
         }
 
+    ##########################################################
+    # Handles cleaning data of form / checking accurate data
+    ##########################################################
     def clean(self):
 
+        # initial clean of form data
         f = self.cleaned_data
 
+        # gets some of the form fields
         password1 = f.get("password")
         password2 = f.get("checkpassword")
         latitude = f.get("latitude")
         longitude = f.get("longitude")
 
+        # validate passwords
         if password1 != password2:
             raise ValidationError("ERROR: Passwords are mismatched")
 
+        # validate register email
         if PickupTeam.objects.filter(email=f.get("email")):
             raise ValidationError("ERROR: A Team Captain has already registered this email address")
 
+        # validate teamname
         if PickupTeam.objects.filter(teamname=f.get("teamname")):
             raise ValidationError("ERROR: This team name has already been taken")
 
+        # validate latitude
         if latitude < -90 or latitude > 90:
             raise ValidationError("ERROR: Latitude must be within -90 to 90")
 
+        # validate longitude
         if longitude < -180 or longitude > 180:
             raise ValidationError("ERROR: Longitude must be within -180 to 180")
