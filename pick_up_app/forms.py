8442860300657881a7bd8e4f9571ed72b59922ac
Thaddeus <71716327+Watkins7@users.noreply.github.com>
2022-03-24 14:11:34 -0400

    error handling added
    -added validation handling to the form, when 'user registration' form is submitted for adding new user

--- pick_up_app/forms.py
+++ pick_up_app/forms.py
@@ -1,11 +1,15 @@
+from django import forms
 from django.forms import ModelForm
 
 # Team Table
 from .models import PickupTeam
 
+# Error Handling
+from django.core.exceptions import ValidationError
 
 # Registration Form
 class NewPickupUserForm(ModelForm):
+
     class Meta:
         model = PickupTeam
 
@@ -14,9 +18,9 @@ class NewPickupUserForm(ModelForm):
             'teamname',
             'email',
             'password',
-            'checkpassword'
+            'checkpassword',
             'longitude',
-            'latitude')
+            'latitude',)
 
         # This is what the form displays them as
         labels = {
@@ -25,5 +29,29 @@ class NewPickupUserForm(ModelForm):
             'password': "Password:",
             'checkpassword': "Password Conformation:",
             'longitude': "Longitude:",
-            'latitude': "Latitude:"
+            'latitude': "Latitude:",
         }
+
+    def clean(self):
+
+        f = self.cleaned_data
+
+        password1 = f.get("password")
+        password2 = f.get("checkpassword")
+        latitude = f.get("latitude")
+        longitude = f.get("longitude")
+
+        if password1 != password2:
+            raise ValidationError("ERROR: Passwords are mismatched")
+
+        if PickupTeam.objects.filter(email=f.get("email")):
+            raise ValidationError("ERROR: A Team Captain has already registered this email address")
+
+        if PickupTeam.objects.filter(teamname=f.get("teamname")):
+            raise ValidationError("ERROR: This team name has already been taken")
+
+        if latitude < -90 or latitude > 90:
+            raise ValidationError("ERROR: Latitude must be within -90 to 90")
+
+        if longitude < -180 or longitude > 180:
+            raise ValidationError("ERROR: Longitude must be within -180 to 180")
