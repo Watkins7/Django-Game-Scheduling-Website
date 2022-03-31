4b6e01ce32293a6300a9825bfe3862e5cc03b923
Sianna Serio <sserio3@umbc.edu>
2022-03-31 18:58:05 -0400

    Added comments

--- pick_up_app/views.py
+++ pick_up_app/views.py
@@ -9,15 +9,15 @@ from .forms import NewPickupUserForm
 # Models
 from .models import User
 
-##########################################
-# Create your views here.
-##########################################
+
 def main_page(request):
     # This is just a message for the app's index view page, can be changed later.
     return HttpResponse("You're looking at the default main page.")
 
 
-def home_page(request, username):
+def home_page(request):
+    # List of the top 5 teams in User model database to be displayed on the
+    # team homepage.
     top_teams_list = User.objects.order_by('-mmr_score')[:5]
     context = {'top_teams_list': top_teams_list}
     return render(request, 'pick_up_app/home_page.html', context)
