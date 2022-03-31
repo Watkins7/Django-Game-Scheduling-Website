9686031a450741f08573659512ceabec564afa85
Sianna Serio <sserio3@umbc.edu>
2022-03-30 21:30:42 -0400

    Added homepage view + html template

--- pick_up_app/urls.py
+++ pick_up_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pick_up_app'
 urlpatterns = [
     path('', views.main_page, name='main_page'),
+    path('<username>/', views.home_page, name='home_page'),
 ]
--- pick_up_app/views.py
+++ pick_up_app/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import User
 
 def main_page(request):
     # This is just a message for the app's index view page, can be changed later.
     return HttpResponse("You're looking at the default main page.")
+
+
+def home_page(request, username):
+    top_teams_list = User.objects.order_by('-mmr_score')[:5]
+    context = {'top_teams_list': top_teams_list}
+    return render(request, 'pick_up_app/home_page.html', context)
+    # return HttpResponse("This is the %s home page." % username)
