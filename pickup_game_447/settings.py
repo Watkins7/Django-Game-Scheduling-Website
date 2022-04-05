7517a50835cf634005b304f0544b9416d16a7f97
Thaddeus <watkins7@umbc.edu>
2022-04-05 15:27:54 -0400

    -settings and requirements.txt updated to include "google maps api"

--- pickup_game_447/settings.py
+++ pickup_game_447/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pick_up_app.apps.PickUpAppConfig'
+    'pick_up_app.apps.PickUpAppConfig',
+    'django_google_maps'
 
 
 ]
