c91c77c5072e847757611c06cd0ab484b72753d6
Thaddeus <watkins7@umbc.edu>
2022-04-09 14:39:39 -0400

    -register model changed to have right default longitude

--- pick_up_app/models.py
+++ pick_up_app/models.py
@@ -35,7 +35,7 @@ class PickupTeam(models.Model):
     email = models.EmailField(max_length=100, default='')
     password = models.CharField(max_length=50,default='')
     checkpassword = models.CharField(max_length=50, default='')
-    longitude = models.FloatField(default=76.7100)
+    longitude = models.FloatField(default=-76.7100)
     latitude = models.FloatField(default=39.2543)
 
     teamaccount = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
