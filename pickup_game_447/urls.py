dc42286d87a74f7551ce5dfb4005ec7bfa613bb0
Thaddeus <71716327+Watkins7@users.noreply.github.com>
2022-04-14 13:47:27 -0400

    update per merge requirement
    -this was updated so that my merge could be approved

--- pickup_game_447/urls.py
+++ pickup_game_447/urls.py
@@ -19,7 +19,7 @@ from pick_up_app import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('pick_up_app.urls')),
+    path('pick_up_app/', include('pick_up_app.urls')),
     path('admin/', admin.site.urls),
 ]
 
